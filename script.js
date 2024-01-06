3349b38da601c16f7fd5cb1cadffd820aa7fa80e
Worrick <worrick1717@gmail.com>
2024-01-06 18:52:39 +0545

    main game logic is creaed here

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let turn = "X";
 
 //Function to change the turn
 const changeTurn=()=>{
-    return turn ==="X"?"O":"X";
+    return turn ==="X"? "O" :"X";
 }
 
 //Function to check for a win - this function will check if X wins or 0 wins
@@ -11,16 +11,15 @@ const checkWin=()=>{
 
 }
 //Main Game Logic
-const gameLogin=()=>{
-    let boxes = document.getElementsByClassName("box");
-    Array.from(boxes).forEach(element=>){
-        let boxtext = document.querySelector('.boxtext');
-        boxtext.addEventListener('click',()=>{
-            if(e.innerText === ''){
-                e.innerText = turn;
-                changeTurn();
-                checkWin();
-            }
-        })
-    }
-}
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText ===''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            checkWin();
+            document.getElementsByClassName("info")[0].innerText = "Turn for  " +turn;
+        }
+    })
+})
